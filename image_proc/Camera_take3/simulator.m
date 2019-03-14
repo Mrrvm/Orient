d7ecaa7518c60701b8859771949b8e7b868186c2
Mrrvm <mrrvm@hotmail.com>
2019-03-14 18:57:48 +0000

    bug fixed

--- image_proc/Camera_take3/simulator.m
+++ image_proc/Camera_take3/simulator.m
@@ -1,4 +1,4 @@
-function [M1, M2, m1, m2] = simulator(N, d, R, T)
+function [M1, M2, m1, m2] = simulator(N, R, T, maxd, mind)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Input
 %   angles  Rotation angles per axis
@@ -11,17 +11,24 @@ function [M1, M2, m1, m2] = simulator(N, d, R, T)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Generate random 3D points
-M1 = (d-0.05)*rand([3,N*5])+0.05;
-
+M1 = (maxd-mind)*rand([3,N*5])+mind;
 % Rotate points
 M2 = R*M1;
 % Translate points
 M2 = M2+T;
-
+% Keep the points with positive depth only
 keep = M2(3, :) > 0;
-
 M1 = M1(:, keep);
 M2 = M2(:, keep);
+% Guarantee there are at least N points with positive depth
+while size(M1, 2) < N
+    M1 = (maxd-mind)*rand([3,N*5])+mind;
+    M2 = R*M1;
+    M2 = M2+T;
+    keep = M2(3, :) > 0;
+    M1 = M1(:, keep);
+    M2 = M2(:, keep);
+end
 
 M1 = M1(1:3, 1:N); 
 M2 = M2(1:3, 1:N); 
